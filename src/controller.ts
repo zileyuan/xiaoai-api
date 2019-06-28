170b398385982357dd52474de5bea54e2f3e3ba1
zileyuan <zileyuan@qq.com>
2019-06-28 19:32:25 +0800

    check game

--- src/controller.ts
+++ src/controller.ts
@@ -1,4 +1,5 @@
 import util from './util';
+import axios from "axios";
 
 export default class Controller {
     constructor() { }
@@ -13,9 +14,23 @@ export default class Controller {
         const requestType=json_data.request.intent.request_type; //Start,Intent,End
         var message;//我应答的话
         if(requestType==="Start"){
-            message=util.buildResponseSimple("演出开始了",false);
+            message=util.buildResponseSimple("客官，需要什么服务",false);
         }else{
-            message=util.buildResponse(["这是一个例子","播放完成后退出"],true);
+            if (query.indexOf('人数') > -1) {
+                let res = await axios.get('https://api.jinzhanwangluo.cn/game-egg/egg/state');
+                if (res.status === 200) {
+                    let amount = res.data.data.roomAmount;
+                    if (amount) {
+                        message=util.buildResponseSimple("客官，目前没人玩游戏哦",false);
+                    } else {
+                        message=util.buildResponseSimple(`客官，只有${amount}个人玩游戏哦`,false);
+                    }
+                } else {
+                    message=util.buildResponseSimple("客官，没有查询到游戏人数哦",false);
+                }
+            } else {
+                message = util.buildResponse(["这是一个例子", "播放完成后退出"], false);
+            }
         }
         console.log(message);
         ctx.body = message;
